2aa192b280180aef769d8cdb1c8dbdbf013b1354
dang11-ly <layaoamandayvelle@gmail.com>
2024-10-04 21:54:15 +0800

    Add files via upload

---
+++ Grade.js
@@ -0,0 +1,10 @@
+let score = prompt(90);
+if (score >= 90) {
+  console.log("Excellent");
+} else if (score >= 80 && score <= 89) {
+  console.log("Good");
+} else if (score >= 70 && score <= 79) {
+  console.log("Fair");
+} else {
+  console.log("Needs Improvement");
+}
